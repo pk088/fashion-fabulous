d9ed1c62ddf9c92fdab11c8bb86eac456450b6f7
pk088 <prajwalkharat88@gmail.com>
2024-04-19 07:13:36 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+          <p>Here is the e commerce website and it is not the only in the box of the </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
